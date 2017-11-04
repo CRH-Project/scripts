73765c1f26c1f27c3e09d74978c55349b15aca58
ApostaC <Aposta@pku.edu.cn>
2017-11-04 19:25:32 +0800

    update...

--- rename.sh
+++ rename.sh
@@ -1,8 +1,30 @@
-SUFFIX=.pcap
-for FILE in ./*; do
-	if [[ ${FILE} != *.pcap ]]; then
-		if [[ ${FILE} != *.sh ]]; then
-			mv ${FILE} ${FILE}${SUFFIX}
+recurse(){
+	DIR=$1
+	for FILE in ${DIR}/*; do
+		if [[ ${FILE} != *.pcap ]]; then
+			if [[ ${FILE} != *.sh ]]; then
+				if [[ -f ${FILE} ]]; then 
+					mv ${FILE} ${FILE}${SUFFIX}
+				elif [[ -d ${FILE} ]]; then 
+					recurse ${FILE}
+				fi
+			fi
 		fi
-	fi
-done	
+	done	
+}
+
+SUFFIX=.pcap
+START_DIR=.
+if [[ -n $1 ]]; then
+	START_DIR=$1
+fi
+recurse ${START_DIR}
+
+
+#for FILE in ./*; do
+#	if [[ ${FILE} != *.pcap ]]; then
+#		if [[ ${FILE} != *.sh ]]; then
+#			mv ${FILE} ${FILE}${SUFFIX}
+#		fi
+#	fi
+#done	
