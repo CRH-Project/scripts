fc4097c81238e308dbd45980353d007255ddc649
ApostaC <Aposta@pku.edu.cn>
2018-01-15 20:25:37 +0800

    merge http and https

--- merge.sh
+++ merge.sh
@@ -10,8 +10,10 @@ NAMES=(
 )
 
 TARGET_ROOT_DIR=total
+BIN_PATH=/media/xia/Soar1/tcpdump-new/analysiser/bin
 mkdir -p $TARGET_ROOT_DIR
 
+#MERGE FLOW DATA
 for CURR_DIR in ./*-anal; do
 	for CURR_FILE in ${NAMES[@]}; do
 		if [ -f $CURR_DIR/$CURR_FILE ]; then
@@ -21,3 +23,10 @@ for CURR_DIR in ./*-anal; do
 		fi 
 	done
 done
+
+#MERGE HTTP AND HTTPS DATA
+touch dir.txt
+ls | awk '{print i$0}' i=`pwd`'/' | grep anal > dir.txt
+${BIN_PATH}/merge_data dir.txt http 2&>1 > /dev/null
+${BIN_PATH}/merge_data dir.txt https 2$>1 > /dev/null 
+
